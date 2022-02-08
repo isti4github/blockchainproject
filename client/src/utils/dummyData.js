b5dc52521a3ee902fa622f1216ccf75e8d6215b6
isti4github <isti.ru.cse@gmail.com>
2022-02-08 13:56:37 +0900

    finished

--- client/src/utils/dummyData.js
+++ client/src/utils/dummyData.js
@@ -45,13 +45,5 @@ export default [
     amount: "0.01",
     addressTo: "0x8aa395Ab97837576aF9cd6946C79024ef1acfdbE",
   },
-  {
-    id: 6,
-    url: "https://www.omnisend.com/blog/wp-content/uploads/2016/09/funny-gifs-9.gif",
-    message: "",
-    timestamp: "12/21/2021, 4:33:21 PM",
-    addressFrom: "0xCF8e569A97C423952DdFf902375C7C76549A6A90",
-    amount: "0.01",
-    addressTo: "0x8aa395Ab97837576aF9cd6946C79024ef1acfdbE",
-  },
+
 ];
